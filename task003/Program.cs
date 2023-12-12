b17ab4f7c2e598ed6a2f8fada47051471e2d3965
BadSantaNF <zeronfmss@gmail.com>
2023-12-12 22:03:10 +0300

    task3

---
+++ task003/Program.cs
@@ -0,0 +1,39 @@
+﻿//Задайте произвольный массив. Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
+
+using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        int[] numbers = { 1, 2, 3, 5, 4, 6, 4, 73, 8, 3 };
+        ReversePrint(numbers);
+    }
+
+    public static void ReversePrint(int[] numbers)
+    {
+        if (numbers.Length == 1)
+        {
+            Console.WriteLine(numbers[0]);
+            return;
+        }
+
+        int lastIndex = numbers.Length - 1;
+        int firstIndex = 0;
+
+        while (lastIndex >= firstIndex)
+        {
+            int temp = numbers[firstIndex];
+            numbers[firstIndex] = numbers[lastIndex];
+            numbers[lastIndex] = temp;
+
+            lastIndex--;
+            firstIndex++;
+        }
+
+        foreach (int number in numbers)
+        {
+            Console.Write(number + " ");
+        }
+    }
+}
